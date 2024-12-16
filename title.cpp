19ed53719f0c1fb6afe3f93a25401ae55ae42bdf
shupopo-999 <shu999.20099@gmail.com>
2024-12-16 13:48:37 +0900

    a

--- title.cpp
+++ title.cpp
@@ -34,7 +34,7 @@ void Title::Init()
 
 
 
-	// ���_�o�b�t�@����
+	// 頂点バッファ生成
 	D3D11_BUFFER_DESC bd{};
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(VERTEX_3D) * 4;
@@ -47,7 +47,7 @@ void Title::Init()
 	Renderer::GetDevice()->CreateBuffer(&bd, &sd, &m_VertexBuffer);
 
 
-	// �e�N�X�`���ǂݍ���
+	// テクスチャ読み込み
 	TexMetadata metadata;
 	ScratchImage image;
 	LoadFromWICFile(L"asset\\texture\\title.png", WIC_FLAGS_NONE, &metadata, image);
@@ -61,7 +61,7 @@ void Title::Init()
 
 }
 
-void Title::UnInit() 
+void Title::UnInit()
 {
 	m_VertexBuffer->Release();
 	m_Texture->Release();
@@ -89,39 +89,39 @@ void Title::Update()
 
 void Title::Draw()
 {
-	// ���̓��C�A�E�g�ݒ�
+	// 入力レイアウト設定
 	Renderer::GetDeviceContext()->IASetInputLayout(m_VertexLayout);
 
-	// �V�F�[�_�ݒ�
+	// シェーダ設定
 	Renderer::GetDeviceContext()->VSSetShader(m_VertexShader, NULL, 0);
 	Renderer::GetDeviceContext()->PSSetShader(m_PixelShader, NULL, 0);
 
-	// �}�g���b�N�X�ݒ�
+	// マトリックス設定
 	Renderer::SetWorldViewProjection2D();
 
-	// �}�e���A���ݒ�
+	// マテリアル設定
 	MATERIAL material;
 	ZeroMemory(&material, sizeof(material));
 	material.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, m_alpha);
 	material.TextureEnable = true;
 	Renderer::SetMaterial(material);
 
-	// ���_�o�b�t�@�ݒ�
+	// 頂点バッファ設定
 	UINT stride = sizeof(VERTEX_3D);
 	UINT offset = 0;
 	Renderer::GetDeviceContext()->IASetVertexBuffers(0, 1, &m_VertexBuffer, &stride, &offset);
 
-	// �e�N�X�`���ݒ�
+	// テクスチャ設定
 	Renderer::GetDeviceContext()->PSSetShaderResources(0, 1, &m_Texture);
 
-	// �v���~�e�B�u�g�|���W�ݒ�
+	// プリミティブトポロジ設定
 	Renderer::GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
-	// �|���S���`��
+	// ポリゴン描画
 	Renderer::GetDeviceContext()->Draw(4, 0);
 }
 
-void Title::SetTitle(float posX,float posY,float sizeX, float sizeY) {
+void Title::SetTitle(float posX, float posY, float sizeX, float sizeY) {
 	VERTEX_3D vertex[4];
 
 	vertex[0].Position = XMFLOAT3(posX - sizeX, posY - sizeY, 0.0f);
@@ -134,7 +134,7 @@ void Title::SetTitle(float posX,float posY,float sizeX, float sizeY) {
 	vertex[1].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 	vertex[1].TexCoord = XMFLOAT2(1.0f, 0.0f);
 
-	vertex[2].Position = XMFLOAT3(posX - sizeX, posY + sizeY,  0.0f);
+	vertex[2].Position = XMFLOAT3(posX - sizeX, posY + sizeY, 0.0f);
 	vertex[2].Normal = XMFLOAT3(0.0f, 0.0f, 0.0f);
 	vertex[2].Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 	vertex[2].TexCoord = XMFLOAT2(0.0f, 1.0f);
@@ -145,7 +145,8 @@ void Title::SetTitle(float posX,float posY,float sizeX, float sizeY) {
 	vertex[3].TexCoord = XMFLOAT2(1.0f, 1.0f);
 
 
-	// ���_�o�b�t�@����
+
+	// 頂点バッファ生成
 	D3D11_BUFFER_DESC bd{};
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(VERTEX_3D) * 4;
@@ -156,4 +157,5 @@ void Title::SetTitle(float posX,float posY,float sizeX, float sizeY) {
 	sd.pSysMem = vertex;
 
 	Renderer::GetDevice()->CreateBuffer(&bd, &sd, &m_VertexBuffer);
+
 }
